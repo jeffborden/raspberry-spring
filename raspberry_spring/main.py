38910d7ec1a2333397aa409051175bb63c353ca6
Brad <bdettmer@gmail.com>
2017-04-07 14:51:33 -0400

    added LEDs

--- raspberry_spring/main.py
+++ raspberry_spring/main.py
@@ -16,18 +16,15 @@ if __name__ == '__main__':
     client = PagerDutyClient()
     atexit.register(exit_handler)
 
-    # TODO remove this or something later.. this is just to show us it's working
-    print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True, service_id='PNN\
-RR6Q'))
-    print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True))
-
-    red_light_pin = 11 # GPIO 17
-
+    red_light = 11      # GPIO 17
+    green_led = 15      # GPIO 15
+    red_led = 31        # GPIO 6
+    
     while True:
         if client.light_should_be_on():
-            pi.on(red_light_pin)
+            pi.on(red_light)
         else:
-            pi.off(red_light_pin)
+            pi.off(red_light)
         time.sleep(10)
 
 

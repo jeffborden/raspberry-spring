08b765d21b8fbaaa507d5ae93bf9de695924889a
Brad <bdettmer@gmail.com>
2017-04-07 14:15:23 -0400

    refactored

--- raspberry_spring/main.py
+++ raspberry_spring/main.py
@@ -1,34 +1,33 @@
 import time
 
-from raspberry_light import RaspberryLight
+from raspberry_pi import RaspberryPi
 from pager_duty_client import PagerDutyClient
 
 import atexit
 
 
+
 def exit_handler():
-    print('My application is ending!')
     pi.clean_up()
-    print('cleaned up pi')
 
 
-atexit.register(exit_handler)
-pi = RaspberryLight(11)
-client = PagerDutyClient()
-    
-# TODO remove this or something later.. this is just to show us it's working
-print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True, service_id='PNN\
+if __name__ == '__main__':
+    pi = RaspberryPi()
+    client = PagerDutyClient()
+    atexit.register(exit_handler)
+
+    # TODO remove this or something later.. this is just to show us it's working
+    print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True, service_id='PNN\
 RR6Q'))
+    print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True))
 
-print(client.get_outstanding_pager_duty_alerts_for_service(verbose=True))
+    red_light_pin = 11 # GPIO 17
 
-
-
-while True:
-    if client.light_should_be_on():
-        pi.high()
-    else:
-        pi.low()
-    time.sleep(10)
+    while True:
+        if client.light_should_be_on():
+            pi.on(red_light_pin)
+        else:
+            pi.off(red_light_pin)
+        time.sleep(10)
 
 
